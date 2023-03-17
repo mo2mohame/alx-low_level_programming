0094d4335825d9183855b640f5486fe43c85cdbb
momen mohamed <momen7607@gmail.com>
2023-03-17 10:42:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
  * Return: Always 0 (Success)
  */
@@ -11,4 +12,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
