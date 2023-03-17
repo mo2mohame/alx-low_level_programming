249765c4854959d17820b7fb843c92646fb7a72e
momen mohamed <momen7607@gmail.com>
2023-03-17 11:59:15 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *this is my comment
+ *
+ *
+ */
+
+int main(void)
+{
+	printf("Size a of char: %d byte(s)\n", (int)sizeof(char));
+	printf("Size a of int: %d byte(s)\n", (int)sizeof(int));
+	printf("Size a of long int: %d byte(s)\n", (int)sizeof(long int));
+	printf("Size a of long long int: %d byte(s)\n", (int)sizeof(long long int));
+	printf("Size a of double: %d byte(s)\n", (int)sizeof(double));
+	printf("Size a of float: %d byte(s)\n", (int)sizeof(float));
+	return (0);
+}
