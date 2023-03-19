5f50a0d1199a2eab4b8d1c93cd688668e04d764d
momen mohamed <momen7607@gmail.com>
2023-03-19 08:04:44 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-    int i = 97;
-    while (i < 123)
-    {
-        putchar(i);
-        i++;
-    }
+	int i = 97;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
 
-    putchar(10);
-    return (0);
+	putchar(10);
+	return (0);
 }
