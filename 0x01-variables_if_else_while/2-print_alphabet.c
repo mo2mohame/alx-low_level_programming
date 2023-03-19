78512ac9e5ff9f9d81efc87e99d717fc13d0a0ec
momen mohamed <momen7607@gmail.com>
2023-03-19 07:47:18 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - function tolower
+ *
+ * Describtion: replace the char from upper to lower
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = tolower('M');
+	putchar(ch);
+	return (0);
+}
