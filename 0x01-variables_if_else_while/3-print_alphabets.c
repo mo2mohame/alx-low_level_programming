d6dd04ba910e01f26542a34518ad72ff478a08ea
momen mohamed <momen7607@gmail.com>
2023-03-20 10:10:58 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - alpha
+ *
+ * Description: alpha fun
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+	while(i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while(j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+	return (0);
+}
