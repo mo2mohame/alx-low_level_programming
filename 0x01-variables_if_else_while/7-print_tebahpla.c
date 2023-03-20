f5a95b29d806120ee91a758cd43ce35089b47e93
momen mohamed <momen7607@gmail.com>
2023-03-20 11:40:17 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - fsaf
+ *
+ * Description: fdsgsdgs
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 123;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+
+	putchar(10);
+
+	return (0);
+}
