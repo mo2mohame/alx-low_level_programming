291a26590560da645b79dc1b9fcc100d7864cd16
momen mohamed <momen7607@gmail.com>
2023-03-20 11:33:44 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - flsanf
+ *
+ * Description: dsfsdgsdg
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
