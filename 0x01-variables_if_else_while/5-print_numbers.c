c2be9d7593d7df04790474cb90db016fe2821bdd
momen mohamed <momen7607@gmail.com>
2023-03-20 11:22:37 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  main - lll
+ *
+ *  Description: fsdafdfa
+ *  
+ *  Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <10)
+	{
+		printf("%d",i);
+		i++;
+	}
+	return (0);
+}
