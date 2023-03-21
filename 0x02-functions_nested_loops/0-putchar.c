a6e358ff5f5576758d6938208f5f49c54932919d
momen mohamed <momen7607@gmail.com>
2023-03-21 10:48:20 -0700

    _putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - gg
- *
- * Description: fdsgsd
- *
- * Return: 0 (Success)
+ * main - gdg
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("_putchar");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
