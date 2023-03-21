e8b4a4244b30833adb85fd15a05c9e911d58ad20
momen mohamed <momen7607@gmail.com>
2023-03-21 14:59:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * void - print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i = 0;
+	
+	while (i < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
