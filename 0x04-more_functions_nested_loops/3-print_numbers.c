5db6a15b063eb5579e20a5dfe0e50b18cea00f5a
momen mohamed <momen7607@gmail.com>
2023-03-24 07:37:01 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_numbers - dsagsdg
+ * print_numbers - prints numbers 0-9
  *
  * Return: void
  */
 
 void print_numbers(void)
 {
-	int i = 0;
+	int c;
 
-	while (i < 10)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
-
-		i++;
+		_putchar(c);
 	}
-
 	_putchar('\n');
 }
