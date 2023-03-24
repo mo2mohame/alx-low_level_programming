0e6cf1aff4166e39364c51cd4743dc93dc1fca7a
momen mohamed <momen7607@gmail.com>
2023-03-24 07:28:16 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - dsagsdg
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i =0;
+	while (i < 10)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
