a2bf52f9a3519fc0260caa8fa12716e9fe766eff
momen mohamed <momen7607@gmail.com>
2023-03-24 07:15:30 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digit
+ * @c: is an int
+ *
+ * Return: 0, 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
