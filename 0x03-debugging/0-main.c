beb0024a35412596addcbe0af2f874613f983ef9
momen mohamed <momen7607@gmail.com>
2023-03-24 18:06:24 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,7 +4,6 @@
 * main - Test function for positive or negative
 * Return: 0
 */
-
 int main(void)
 
 {
@@ -12,7 +11,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 	return (0);
 }
