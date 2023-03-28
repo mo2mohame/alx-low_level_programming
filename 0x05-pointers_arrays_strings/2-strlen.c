6474ea435e47a17653c825b2ae04c5be65aa1b57
momen mohamed <momen7607@gmail.com>
2023-03-28 16:42:40 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - dg
+ * @s:gfsg
+ *
+ * Return: gfsg
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return( len);
+}
