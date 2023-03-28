e567ad5ad43947e18b81bea00885747be0d83262
momen mohamed <momen7607@gmail.com>
2023-03-28 16:37:01 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - djckj
+ * @a: int
+ * @b: int
+ *
+ * Return: dsg
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x = *a;
+	*a = *b;
+	*b = x;
+}
