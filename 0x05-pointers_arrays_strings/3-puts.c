0162d66c8abe87b2267311a751919a282f927da6
momen mohamed <momen7607@gmail.com>
2023-03-28 17:13:02 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "mian.h"
+
+/**
+ * _puts - gkjds
+ * @str:fsg
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
